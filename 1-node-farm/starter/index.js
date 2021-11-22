d0a3322979c9be696f37d4cbaa2fce3a8127d5f4
Ross Acheson <ross.acheson@gmail.com>
2021-11-22 18:40:39 -0500

    Pull in third-party module slugify; use nodemon for continuous dev

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -10,6 +12,9 @@ const tempProduct= fs.readFileSync(`${__dirname}/templates/template-product.html
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(p => slugify(p.productName, { lower: true }));
+console.log(slugs);
 
 
 const server = http.createServer((req, res) => {
