6fdefeb701e678800ad3057e3a67084ab74637ec
Ross Acheson <ross.acheson@gmail.com>
2021-11-22 16:46:50 -0500

    Add api route and return products json

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
@@ -10,7 +14,12 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
-    } else {
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+    }  else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
