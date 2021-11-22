fda383e7eced6cd7d97477d6270ba92b47356a7d
Ross Acheson <ross.acheson@gmail.com>
2021-11-22 18:16:26 -0500

    Move replaceTemplate function into another file/module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,22 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-
-    return output;
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -25,7 +10,6 @@ const tempProduct= fs.readFileSync(`${__dirname}/templates/template-product.html
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
 
 
 const server = http.createServer((req, res) => {
