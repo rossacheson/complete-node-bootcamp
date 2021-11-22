2c43a17f8e202b3d471f51ac23ea7ae1f45f0412
Ross Acheson <ross.acheson@gmail.com>
2021-11-22 15:23:28 -0500

    Some simple home-grown routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,22 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!');
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
